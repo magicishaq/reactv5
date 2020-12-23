7c59e55796d25fac230c7f72b9e7f4711f73610e
Ishaq Khan <ikhan@uk.nex.global>
2020-12-23 12:05:35 +0000

    finished lint

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,15 +1,43 @@
- //code goes here
-const Pet = () => {
-    return React.createElement('div', {id: 'pet'},[
-        React.createElement('h1', {} , 'Paz'), 
-        React.createElement('h2', {} , 'Mylow'), 
-        React.createElement('h2', {} , 'DeeDee')
-    ])
-}
+//passes in te props object
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement(
+    "div",
+    {
+      id: "pet",
+    },
+    [
+      React.createElement("h1", {}, name),
+      React.createElement("h2", {}, animal),
+      React.createElement("h2", {}, breed),
+    ]
+  );
+};
 
- const App = () => {
-    return React.createElement('div', {}, React.createElement('h1', {id: 'ishaq'}, 'Adopt Me')) 
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement(
+      "h1",
+      {
+        id: "ishaq",
+      },
+      "Adopt Me"
+    ),
+    React.createElement(Pet, {
+      name: "Paz",
+      animal: "Dog",
+      breed: "Malamute",
+    }),
+    React.createElement(Pet, {
+      name: "Mylow",
+      animal: "Dog",
+      breed: "Jack russel",
+    }),
+    React.createElement(Pet, {
+      name: "DeeDee",
+      animal: "Dog",
+      breed: "Staffy",
+    }),
+  ]);
+};
 
-}
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
